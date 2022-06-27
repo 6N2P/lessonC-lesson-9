a3324612d4e20deccec6e3c2c867767c78348fb9
OITtmnkonstr <oit@tmnkonst.ru>
2022-06-27 16:33:37 +0500

    redid task 2

--- Lesson_9.2/Program.cs
+++ Lesson_9.2/Program.cs
@@ -19,7 +19,7 @@ namespace Lesson_9._2
         static TelegramBotClient bot;
         static void Main(string[] args)
         {
-            string token = File.ReadAllText(@"H:\C#lesson\lesson_9\token.txt");
+            string token = File.ReadAllText(@"C:\Users\Ivanovsv\Desktop\Lessons\Lesson_9\token.txt");
 
             bot = new TelegramBotClient(token);
             bot.OnMessage += MessageList;
@@ -27,114 +27,93 @@ namespace Lesson_9._2
             Console.ReadKey();
             
         }
-        private static void MessageList(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        private static void MessageListener(object sender, Telegram.Bot.Args.MessageEventArgs e)
         {
-            string pathDownLoadFail = @"H:\C#lesson\lesson_9\DownLoad\";
             string text = $"{DateTime.Now.ToLongTimeString()}: {e.Message.Chat.FirstName} {e.Message.Chat.Id} {e.Message.Text}";
-
             Console.WriteLine($"{text} TypeMessage: {e.Message.Type.ToString()}");
 
+            if (e.Message.Text == "/start")
+            {
+                bot.SendTextMessageAsync(e.Message.Chat.Id,
+               $"Для просмотра папки с файлами отправте сообщение show-f, для скачивания файла d-f-'номер файла'  ");
+            }
+
+            //сохранение документа
             if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Document)
             {
                 Console.WriteLine(e.Message.Document.FileId);
                 Console.WriteLine(e.Message.Document.FileName);
                 Console.WriteLine(e.Message.Document.FileSize);
 
-                DownLoad(e.Message.Document.FileId, pathDownLoadFail+ e.Message.Document.FileName);
+                DownLoad(e.Message.Document.FileId, e.Message.Document.FileName);
+                return;
             }
 
-            if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Photo)
-            {
-                
-                Console.WriteLine(e.Message.Photo[e.Message.Photo.Length - 1].FileId);
-                Console.WriteLine(e.Message.Photo.Length);
-                Console.WriteLine(e.Message.Photo[e.Message.Photo.Length - 1].FileSize);                
-
-                DownLoad(e.Message.Photo[e.Message.Photo.Length - 1].FileId, pathDownLoadFail + e.Message.Photo[e.Message.Photo.Length - 1].FileSize + ".jpg");
-            }
-
+            //сохранение аудио
             if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Audio)
             {
                 Console.WriteLine(e.Message.Audio.FileId);
                 Console.WriteLine(e.Message.Audio.FileName);
                 Console.WriteLine(e.Message.Audio.FileSize);
 
-                DownLoad(e.Message.Audio.FileId, pathDownLoadFail + e.Message.Audio.FileName);
-            }
-
-            if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Video)
-            {
-                Console.WriteLine(e.Message.Video.FileId);
-                Console.WriteLine(e.Message.Video.FileName);
-                Console.WriteLine(e.Message.Video.FileSize);
-
-                string path2;
-
-                if (e.Message.Video.FileName != null)
-                {
-                    path2 = e.Message.Video.FileName;
-                }
-                else
-                {
-                    path2 =Convert.ToString( e.Message.Video.FileSize)+".mp4";
-                }
-
-                DownLoad(e.Message.Video.FileId, pathDownLoadFail + path2);
-            }
-
-            if (e.Message.Text == "/start")
-            {
-                string messgeText = "Для просмотра файлов нажмите 1\nДля скачивания файлов нажмите2";
-                bot.SendTextMessageAsync(e.Message.Chat.Id, $"{messgeText}");
-            }
-
-            if (e.Message.Text == "1")
-            {
-                string messgeText = ShowContent();
-                bot.SendTextMessageAsync(e.Message.Chat.Id, $"{messgeText}");
-            }
-
-            bool flag = true;
-
-            //if (e.Message.Text == "2")
-            //{              
-
-            //    string messgeText = ShowContent();
-            //    string messgeText2 = "Какой файл вы хотите скачать?\nВыберите строку\nДля выхожа нажмите 'q'";
-            //    bot.SendTextMessageAsync(e.Message.Chat.Id, $"{messgeText}");
-            //    bot.SendTextMessageAsync(e.Message.Chat.Id, $"{messgeText2}");
-            //    if (e.Message.Text == "q")
-            //    {
-            //        flag = false;
-            //    }
-            //    try
-            //    {
-            //        if (flag)
-            //        {
-            //            bot.OnMessage += MessageListDownLoad;                        
-
-            //        }
-            //        else
-            //        {
-            //            bot.OnMessage -= MessageListDownLoad;
-            //        }
-            //    }
-            //    catch { }
-                
-            //}
-
-            if (e.Message.Text == null)
-            {
+                DownLoad(e.Message.Audio.FileId, e.Message.Audio.FileName);
 
                 return;
             }
-            else
+
+            //сохранение картинки
+            if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Photo)
             {
-                var messgeText = $" {e.Message.Text} {e.Message.Chat.FirstName}";
-                bot.SendTextMessageAsync(e.Message.Chat.Id, $"{messgeText}");
+                Console.WriteLine(e.Message.Photo[3].FileId);
+
+                Console.WriteLine(e.Message.Photo[3].FileSize);
+
+                DownLoad(e.Message.Photo[3].FileId, $"{e.Message.Photo[3].FileSize}.jpg");
+                return;
+            }
+
+            if (e.Message.Text == "show-f")
+            {
+                string listFile = ShowContent();
+                bot.SendTextMessageAsync(e.Message.Chat.Id, $"{listFile}");
+
+                return;
             }
 
 
+            if (e.Message.Text.StartsWith("d-f-"))
+            {
+                //получаю массив названий файлов в папке
+                string[] masiveFails = MasiveFiles();
+                //получаю номер строки из массива
+                int numberString = Convert.ToInt32(e.Message.Text.Substring(4));
+
+                try
+                {
+                    string nameFail = masiveFails[numberString - 1];
+
+                    using (var strim = File.OpenRead(nameFail))
+                    {
+                        InputOnlineFile iof = new InputOnlineFile(strim);
+                        iof.FileName = nameFail;
+                        string iofType = Path.GetExtension(nameFail);
+
+                        if (iofType == ".jpg")
+                        {
+                            var sendPhote = bot.SendPhotoAsync(e.Message.Chat.Id, iof);
+                        }
+
+                        bot.SendDocumentAsync(e.Message.Chat.Id, iof);
+
+                        bot.SendTextMessageAsync(e.Message.Chat.Id, "отправлен файл");
+
+
+                    }
+
+                }
+                catch { }
+                return;
+            }
 
         }
 
@@ -172,41 +151,53 @@ namespace Lesson_9._2
             }
         }
 
+        /// <summary>
+        /// загружает файл
+        /// </summary>
+        /// <param name="fileId"></param>
+        /// <param name="path"></param>
         static async void DownLoad(string fileId, string path)
         {
-            var file = await bot.GetFileAsync(fileId);
-            FileStream fs = new FileStream( path, FileMode.Create);
-            await bot.DownloadFileAsync(file.FilePath, fs);
-            fs.Close();
-
-            fs.Dispose();
-        }
-
-        static  string ShowContent()
-        {
-            string content = string.Empty;
-            string corectContent = string.Empty;
-            string[] masiveFails = MasiveFiles();
-
-            int numberString = 1;
-            for (int i = 0; i < masiveFails.Length; i++)
+            try
             {
-                //удоляю из строики часть где прописан путь к файлам
-                corectContent = masiveFails[i].Remove(0, 30);
-                //записываю всё в строку
-                content = $"{content}{i+numberString} {corectContent}\n";
-            }
-            numberString = 1;
+                var file = await bot.GetFileAsync(fileId);
+                FileStream fs = new FileStream(@"C:\Users\Ivanovsv\Desktop\Lessons\Lesson_9\DownLoad\" + path, FileMode.Create);
+                await bot.DownloadFileAsync(file.FilePath, fs);
 
-            return content;
+                fs.Close();
+                fs.Dispose();
+            }
+            catch { }
+        }
+        /// <summary>
+        /// показывает содержимое закаченной папки
+        /// </summary>
+        /// <returns></returns>
+        static string ShowContent()
+        {
+            string result = string.Empty;
+
+            string[] listFile = MasiveFiles();
+
+            int counter = 1;
+            foreach (var v in listFile)
+            {
+                string text = v.Remove(0, 52);
+                result += $"{counter} {text} \n";
+
+                counter++;
+            }
+
+            return result;
         }
 
-        static string [] MasiveFiles ()
+        static string[] MasiveFiles()
         {
-            string[] masiveFails = Directory.GetFiles(@"H:\C#lesson\lesson_9\DownLoad");
+            string directoryName = @"C:\Users\Ivanovsv\Desktop\Lessons\Lesson_9\DownLoad\";
+            string[] masiveFails = Directory.GetFiles(directoryName);
             return masiveFails;
         }
 
-       
+
     }
 }
